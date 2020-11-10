136c64e021e7abae31430672d5cbb0e216a3d82d
郭勇 <>
2020-11-10 15:32:18 +0800

    badgeTextView

---
+++ app/build/generated/source/buildConfig/debug/ezy/demo/badgebutton/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ezy.demo.badgebutton;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ezy.demo.badgebutton";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ library/build/generated/source/buildConfig/debug/ezy/library/badgebutton/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ezy.library.badgebutton;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "ezy.library.badgebutton";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.1.0";
+}
